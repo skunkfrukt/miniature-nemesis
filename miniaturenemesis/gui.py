a93fa94a2b610d41f5fbf776da5bb69528392d13
Namida Aneskans <namida@skunkfrukt.se>
2012-09-02 13:16:37 +0200

    Added unit tests that don't work.

--- miniaturenemesis/gui.py
+++ miniaturenemesis/gui.py
@@ -6,6 +6,40 @@ guy = pyglet.sprite.Sprite(pyglet.resource.image('png/testguy.png'))
 guy.dx = 0.0
 guy.dy = 0.0
 
+class GameMenu:
+    def __init__(self,options, initial_option = 0, wrap = True):
+        self.options = options
+        self.selected_index = initial_option
+        self.wrap_around = wrap
+    
+    def next_option(self):
+        return self.change_selected_index(1)
+    
+    def previous_option(self):
+        return self.change_selected_index(-1)
+        
+    def change_selected_index(self, di):
+        new_selected_index = self.selected_index + di
+        if self.wrap_around:
+            while new_selected_index < 0:
+                new_selected_index += len(self.options)
+            self.selected_index = new_selected_index % len(self.options)
+        elif new_selected_index in range(len(self_options)):
+            self.selected_index = new_selected_index
+        else:
+            pass # Throw some kind of an error or do nothing, maybe.
+        return self.selected_index
+    
+    def selected_option(self):
+        return self.selected_index
+    
+    def selected_option_name(self):
+        return self.options[self.selected_option()]
+    
+class GameState:
+    def __init__(self):
+        pass
+
 
 class MainWindow(pyglet.window.Window):
     def __init__(self):
