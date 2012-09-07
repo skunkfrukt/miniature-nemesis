f5ee64834190391110224653f331c90eae45b791
Namida Aneskans <namida@skunkfrukt.se>
2012-09-07 21:30:01 +0200

    Made crude collider classes using collision boxes and pixel-size colliders.

---
+++ miniaturenemesis/collider.py
@@ -0,0 +1,36 @@
+class Collider:
+    def __init__(self, x = 0, y = 0, width = 1, height = 1):
+        self.x, self.y = x, y
+        self.width, self.height = width, height
+        
+    def collide(self, other_collider):
+        return collide_x(other_collider) and collide_y(other_collider)
+    
+    def collide_x(self, other_collider):
+        diff = other_collider.x - self.x
+        if diff == 0:
+            return true
+        elif diff < 0:
+            return -diff < other_collider.width
+        else: # diff > 0
+            return diff < self.width
+    
+    def collide_y(self, other_collider):
+        diff = other_collider.y - self.y
+        if diff == 0:
+            return true
+        elif diff < 0:
+            return -diff < other_collider.height
+        else: # diff > 0
+            return diff < self.height
+
+class PointCollider(Collider):
+    def __init__(self,x=0,y=0):
+        super(PointCollider, self).__init__(x, y, 1, 1)
+        
+    def collide(self, other_collider):
+        return self.x == other_collider.x and self.y == other_collider.y
+    
+        
+if __name__ == '__main__':
+    import tests.test_collider
