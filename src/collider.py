4e3f62d8ffe3391671db372a90149b81bd37d210
Namida Aneskans <namida@skunkfrukt.se>
2012-09-26 20:58:49 +0200

    Changed how colliders work internally. Broke everything; yay for tight coupling!

--- src/collider.py
+++ src/collider.py
@@ -1,27 +1,32 @@
 class Collider:
-    def __init__(self, x = 0, y = 0, width = 1, height = 1):
-        self.x, self.y = x, y
-        self.width, self.height = width, height
+    def __init__(self, x0=0, y0=0, x1=None, y1=None, width=None, height=None):
+        self.x0, self.y0 = x0, y0
+        if width is not None:
+            assert x1 is None, "Collider has both width and x1"
+            self.x1 = x0 + width
+        else:
+            assert x1 is not None, "Collider lacks both width and x1"
+            self.x1 = x1
+        if height is not None:
+            assert y1 is None, "Collider has both height and y1"
+            self.y1 = y0 + height
+        else:
+            assert y1 is not None, "Collider lacks both width and y1"
+            self.y1 = y1
+        
         
     def collide(self, other):
-        return self.collide_x(other) and self.collide_y(other) and self.x < other.x
+        return self.collide_x(other) and self.collide_y(other)  # and self.x < other.x
     
     def collide_x(self, other):
-        cx0 = max(self.x, other.x)
-        cx1 = min(self.x + self.width, other.x + other.width)
-        return cx1 > cx0
+        overlap_x0 = max(self.x0, other.x0)
+        overlap_x1 = min(self.x1, other.x1)
+        return overlap_x1 > overlap_x0  # True if overlap has positive width.
         
     def collide_y(self, other):
-        cy0 = max(self.y, other.y)
-        cy1 = min(self.y + self.height, other.y + other.height)
-        return cy1 > cy0
-
-class PointCollider(Collider):
-    def __init__(self,x=0,y=0):
-        super(PointCollider, self).__init__(x, y, 1, 1)
-        
-    def collide(self, other):
-        return self.x == other.x and self.y == other.y
+        overlap_y0 = max(self.y0, other.y0)
+        overlap_y1 = min(self.y1, other.y1)
+        return overlap_y1 > overlap_y0  # True if overlap has positive height.
     
         
 if __name__ == '__main__':
--- tests/test_collider.py
+++ tests/test_collider.py
@@ -4,45 +4,53 @@ import unittest
 class TestCollider(unittest.TestCase):
     def setUp(self):
         # The main collider is the one we'll be testing stuff against.
-        self.collider = Collider(0,0,10,10)
+        # The alternative one is just the same, constructed differently.
+        self.collider = Collider(0,0,width=10,height=10)
+        self.collider_alternative = Collider(0,0,10,10)
         
         # Concentric colliders.
-        self.small_collider = Collider(4,4,2,2)
-        self.large_hadron_collider = Collider(-5,-5,20,20)
+        self.small_collider = Collider(4,4,width=2,height=2)
+        self.large_hadron_collider = Collider(-5,-5,width=20,height=20)
         
         # Colliders to the north and to the south of main.
-        self.north_discrete = Collider(-5,13,20,2)
-        self.north_overlapping = Collider(-5,5,20,10)
-        self.south_discrete = Collider(-5,-5,20,2)
-        self.south_overlapping = Collider(-5,-5,20,10)
+        self.north_discrete = Collider(-5,13,width=20,height=2)
+        self.north_overlapping = Collider(-5,5,width=20,height=10)
+        self.south_discrete = Collider(-5,-5,width=20,height=2)
+        self.south_overlapping = Collider(-5,-5,width=20,height=10)
         
         # Colliders to the east and to the west of main.
-        self.east_discrete = Collider(13,-5,2,20)
-        self.east_overlapping = Collider(5,-5,10,20)
-        self.west_discrete = Collider(-5,-5,2,20)
-        self.west_overlapping = Collider(-5,-5,10,20)
+        self.east_discrete = Collider(13,-5,width=2,height=20)
+        self.east_overlapping = Collider(5,-5,width=10,height=20)
+        self.west_discrete = Collider(-5,-5,width=2,height=20)
+        self.west_overlapping = Collider(-5,-5,width=10,height=20)
         
         # Colliders in the corners of main.
-        self.southwest_discrete = Collider(-5,-5,2,2)
-        self.southwest_overlapping = Collider(-5,-5,10,10)
-        self.southeast_discrete = Collider(13,13,2,2)
-        self.southeast_overlapping = Collider(5,5,10,10)
-        self.northwest_discrete = Collider(-5,13,2,2)
-        self.northwest_overlapping = Collider(-5,5,10,10)
-        self.northeast_discrete = Collider(13,13,2,2)
-        self.northeast_overlapping = Collider(5,5,10,10)
+        self.southwest_discrete = Collider(-5,-5,width=2,height=2)
+        self.southwest_overlapping = Collider(-5,-5,width=10,height=10)
+        self.southeast_discrete = Collider(13,13,width=2,height=2)
+        self.southeast_overlapping = Collider(5,5,width=10,height=10)
+        self.northwest_discrete = Collider(-5,13,width=2,height=2)
+        self.northwest_overlapping = Collider(-5,5,width=10,height=10)
+        self.northeast_discrete = Collider(13,13,width=2,height=2)
+        self.northeast_overlapping = Collider(5,5,width=10,height=10)
         
         # Close colliders.
-        self.north_close = Collider(0,10,10,5)
-        self.south_close = Collider(0,-5,10,5)
-        self.west_close = Collider(-5,0,5,10)
-        self.east_close = Collider(10,0,5,10)
+        self.north_close = Collider(0,10,width=10,height=5)
+        self.south_close = Collider(0,-5,width=10,height=5)
+        self.west_close = Collider(-5,0,width=5,height=10)
+        self.east_close = Collider(10,0,width=5,height=10)
         
     def assertCollides(self, other):
         return self.assertEqual(True, self.collider.collide(other))
         
     def assertDodges(self, other):
         return self.assertEqual(False, self.collider.collide(other))
+        
+    def test_both_notations_work_for_x(self):
+        self.assertEqual(self.collider.x1, self.collider_alternative.x1)
+        
+    def test_both_notations_work_for_y(self):
+        self.assertEqual(self.collider.y1, self.collider_alternative.y1)
         
     def test_small_concentric_collider(self):
         self.assertCollides(self.small_collider)
