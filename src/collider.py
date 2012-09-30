99ff4cc1947f5f18a84d5ab5a3603368192bae0e
Namida Aneskans <namida@skunkfrukt.se>
2012-09-30 18:06:33 +0200

    Renamed x0, y0, x1, y1 to left, bottom, right, top in Collider class.

--- src/collider.py
+++ src/collider.py
@@ -1,41 +1,42 @@
 class Collider:
-    def __init__(self, x0=0, y0=0, x1=None, y1=None, width=None, height=None):
-        self.x0, self.y0, self.x1, self.y1 = None, None, None, None
-        self.offset_x0, self.offset_y0 = x0, y0
+    def __init__(self, left=0, bottom=0, right=None, top=None,
+            width=None, height=None):
+        self.left, self.bottom, self.right, self.top = None, None, None, None
+        self.offset_left, self.offset_bottom = left, bottom
         if width is not None:
-            assert x1 is None, "Collider has both width and x1"
-            self.offset_x1 = x0 + width
+            assert right is None, "Collider has both width and right"
+            self.offset_right = left + width
         else:
-            assert x1 is not None, "Collider lacks both width and x1"
-            self.offset_x1 = x1
+            assert right is not None, "Collider lacks both width and right"
+            self.offset_right = right
         if height is not None:
-            assert y1 is None, "Collider has both height and y1"
-            self.offset_y1 = y0 + height
+            assert top is None, "Collider has both height and top"
+            self.offset_top = bottom + height
         else:
-            assert y1 is not None, "Collider lacks both width and y1"
-            self.offset_y1 = y1
-        assert self.offset_x0 < self.offset_x1, "Collider's x0 >= x1"
-        assert self.offset_y0 < self.offset_y1, "Collider's y0 >= y1"
+            assert top is not None, "Collider lacks both width and top"
+            self.offset_top = top
+        assert self.offset_left < self.offset_right, "Collider's left >= right"
+        assert self.offset_bottom < self.offset_top, "Collider's bottom >= top"
         
     def collide(self, other):
-        assert self.x0 is not None, "Collider's real x0 is None."
-        assert self.y0 is not None, "Collider's real y0 is None."
-        assert self.x1 is not None, "Collider's real x1 is None."
-        assert self.y1 is not None, "Collider's real y1 is None."
+        assert self.left is not None, "Collider's real left is None."
+        assert self.bottom is not None, "Collider's real bottom is None."
+        assert self.right is not None, "Collider's real right is None."
+        assert self.top is not None, "Collider's real top is None."
         return self.collide_x(other) and self.collide_y(other)
     
     def collide_x(self, other):
-        overlap_x0 = max(self.x0, other.x0)
-        overlap_x1 = min(self.x1, other.x1)
-        return overlap_x1 > overlap_x0  # True if overlap has positive width.
+        overlap_left = max(self.left, other.left)
+        overlap_right = min(self.right, other.right)
+        return overlap_right > overlap_left  # True if overlap has positive width.
         
     def collide_y(self, other):
-        overlap_y0 = max(self.y0, other.y0)
-        overlap_y1 = min(self.y1, other.y1)
-        return overlap_y1 > overlap_y0  # True if overlap has positive height.
+        overlap_bottom = max(self.bottom, other.bottom)
+        overlap_top = min(self.top, other.top)
+        return overlap_top > overlap_bottom  # True if overlap has positive height.
         
     def move(self, base_x, base_y):
-        self.x0 = base_x + self.offset_x0
-        self.y0 = base_y + self.offset_y0
-        self.x1 = base_x + self.offset_x1
-        self.y1 = base_y + self.offset_y1
+        self.left = base_x + self.offset_left
+        self.bottom = base_y + self.offset_bottom
+        self.right = base_x + self.offset_right
+        self.top = base_y + self.offset_top
