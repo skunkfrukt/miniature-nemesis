f9d5c972b57da6ca1234d241a28094962ecb20d6
Namida Aneskans <namida@skunkfrukt.se>
2012-10-01 19:56:08 +0200

    Changed variable names in Collider class.

--- src/collider.py
+++ src/collider.py
@@ -2,21 +2,21 @@ class Collider:
     def __init__(self, left=0, bottom=0, right=None, top=None,
             width=None, height=None):
         self.left, self.bottom, self.right, self.top = None, None, None, None
-        self.offset_left, self.offset_bottom = left, bottom
+        self._offset_left, self._offset_bottom = left, bottom
         if width is not None:
             assert right is None, "Collider has both width and right"
-            self.offset_right = left + width
+            self._offset_right = left + width
         else:
             assert right is not None, "Collider lacks both width and right"
-            self.offset_right = right
+            self._offset_right = right
         if height is not None:
             assert top is None, "Collider has both height and top"
-            self.offset_top = bottom + height
+            self._offset_top = bottom + height
         else:
             assert top is not None, "Collider lacks both width and top"
-            self.offset_top = top
-        assert self.offset_left < self.offset_right, "Collider's left >= right"
-        assert self.offset_bottom < self.offset_top, "Collider's bottom >= top"
+            self._offset_top = top
+        assert self._offset_left < self._offset_right, "Collider's left >= right"
+        assert self._offset_bottom < self._offset_top, "Collider's bottom >= top"
         
     def collide(self, other):
         assert self.left is not None, "Collider's real left is None."
@@ -36,7 +36,7 @@ class Collider:
         return overlap_top > overlap_bottom  # True if overlap has positive height.
         
     def move(self, base_x, base_y):
-        self.left = base_x + self.offset_left
-        self.bottom = base_y + self.offset_bottom
-        self.right = base_x + self.offset_right
-        self.top = base_y + self.offset_top
+        self.left = base_x + self._offset_left
+        self.bottom = base_y + self._offset_bottom
+        self.right = base_x + self._offset_right
+        self.top = base_y + self._offset_top
