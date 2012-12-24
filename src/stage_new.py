f0d6d3b33808e67f3b03a4e162d9e00d03353b80
Namida Aneskans <namida@skunkfrukt.se>
2012-12-24 20:49:01 +0100

    Additions to new Stage class.

--- src/stage_new.py
+++ src/stage_new.py
@@ -16,10 +16,20 @@ class Stage(pyglet.event.EventDispatcher):
         self.offset = 0
         self.active_section = None
 
-        log.info('Stage {} initialised.'.format(self.name))
+        log.debug('Initialised Stage {}.'.format(self.name))
 
     def setup(self):
         pass
+
+    def reset(self):
+        self.despawn_props(self.all_props)
+        self.despawn_actors(self.all_actors)
+
+        self.is_scrolling = False
+        self.offset = 0
+        self.active_section = None
+
+        log.info('Reset Stage {}.'.format(self.name))
 
     def update(self, dt):
         if self.is_scrolling:
@@ -30,25 +40,37 @@ class Stage(pyglet.event.EventDispatcher):
         self.update_actors(dt)
 
     def advance_section(self):
+        if self.active_section is not None:
+            self.exit_section(self.active_section)
         try:
             new_section = self.section_iter.next()
+            self.enter_section(new_section)
         except StopIteration:
             self.is_scrolling = False
-            new_section = None
-        finally:
-            self.despawn(self.old_props)
-            self.set_active_section(new_section)
+            self.dispatch_event('on_enter_final_section')
 
-    def set_active_section(self, new_section):
-        old_section = self.active_section
+    def exit_section(self, old_section):
+        self.despawn_props(self.old_props)
         old_section.reset()
+        self.active_section = None
+        self.dispatch_event('on_exit_section', old_section.name)
+
+        log.info('Exited Section {}.'.format(old_section.name))
+
+    def enter_section(self, new_section):
         if new_section is not None:
             new_section.setup()
             self.all_props |= new_section.props
         self.active_section = new_section
+        self.dispatch_event('on_enter_section', new_section.name)
+
+        log.info('Entered Section {}.'.format(new_section.name))
 
     def append_section(self, section):
         self.sections.append(section)
+
+        log.debug('Appended Section {} to Stage {}.'.format(
+                section.name, self.name))
 
     @property
     def current_props(self):
@@ -61,14 +83,18 @@ class Stage(pyglet.event.EventDispatcher):
     def old_props(self):
         return self.all_props - self.current_props
 
-Stage.register_event_type('on_enter_stage')
-Stage.register_event_type('on_exit_stage')
+Stage.register_event_type('on_begin_stage')
+Stage.register_event_type('on_end_stage')
+Stage.register_event_type('on_enter_section')
+Stage.register_event_type('on_exit_section')
 
 
 class StageSection(pyglet.event.EventDispatcher):
     def __init__(self, name):
         self.name = name
         self.props = None
+
+        log.debug('Initialised StageSection {}.'.format(self.name))
 
     def setup(self, offset):
         self.setup_props(offset)
