cf4d0a13700d648b019e709da33ee6f848eaee69
Namida Aneskans <namida@skunkfrukt.se>
2013-02-19 20:27:42 +0100

    Removed some junk code.

--- src/prop.py
+++ src/prop.py
@@ -10,10 +10,3 @@ class Prop(gameobject.GameObject):
     def show(self):
         self.sprite.image = self.image
         super(Prop, self).show()
-
-    '''def setup_sprite(self, batch, group):
-        if self.sprite is not None:
-            if batch is not None:
-                self.sprite.batch = batch
-            if group is not None:
-                self.sprite.group = group'''
